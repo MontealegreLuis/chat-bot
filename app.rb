d3f8ec8eb843730a05b8d89b20a23ccebb8f6c00
Luis Montealegre <montealegreluis@gmail.com>
2017-05-22 21:16:19 -0500

    Move classes to the lib directory.

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
-require_relative 'bot'
-require_relative 'word_play'
+require_relative 'lib/bot'
+require_relative 'lib/word_play'
 
 bot = Bot.new(:name => 'Fred', :data_file => 'fred.bot')
 puts bot.greeting
 
-while input = gets and input.chomp != 'end'
+while input = gets and input.chomp != 'bye'
     puts '>> ' + bot.response_to(input)
 end
 
