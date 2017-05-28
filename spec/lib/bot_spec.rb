15f9c890ef8a7b27d6fdb6ce97aaab249af45d0b
Luis Montealegre <montealegreluis@gmail.com>
2017-05-27 20:24:52 -0500

    Add more examples of responses given by the bot

--- spec/lib/bot_spec.rb
+++ spec/lib/bot_spec.rb
@@ -18,11 +18,37 @@ describe Bot do
         expect { Bot.new({:data_file => 'foo.bot'}) }.to raise_exception(/foo\.bot/)
     end
 
-    it 'produces a random greet' do
+    it 'produces a random greeting' do
         bot = Bot.new({:data_file => @file})
+
         greeting = bot.greeting
         greeting.sub! 'Unnamed bot', '[name]' if greeting.include? 'Unnamed bot'
+
         expect(@writer.knowledge[:responses][:greeting]).to include(greeting)
+    end
+
+    it 'selects a farewell randomly' do
+        bot = Bot.new({:data_file => @file})
+
+        farewell = bot.farewell
+
+        expect(@writer.knowledge[:responses][:farewell]).to include(farewell)
+    end
+
+    it 'selects a default message when given an unknown message' do
+        bot = Bot.new({:data_file => @file})
+
+        default = bot.response_to("I'm an instructor")
+
+        expect(@writer.knowledge[:responses][:default]).to include(default)
+    end
+
+    it 'responds randomly to another greeting' do
+        bot = Bot.new({:data_file => @file})
+
+        response = bot.response_to('Hello again')
+
+        expect(@writer.knowledge[:responses]['hello']).to include(response)
     end
 end
 
